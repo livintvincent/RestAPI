76ae3ae54322798d22c2dac4ea38f33a223d2b6a
livintvincent <75431786+livintvincent@users.noreply.github.com>
2020-12-08 20:12:54 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Address
+from .serializer import AddressSerializer
+
+class AddressViewSet (ModelViewSet):
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
